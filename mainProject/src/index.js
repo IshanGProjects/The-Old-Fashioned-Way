723f2944d44fa122333b61d85727152e4bfeb167
Erik Hirschmann <erhi6821@colorado.edu>
2022-11-03 14:25:11 -0600

    register bug req.body not working

--- mainProject/src/index.js
+++ mainProject/src/index.js
@@ -1,8 +1,9 @@
 //IMPORTING THE DEPENDENCIES
 const express = require('express');
 const bodyParser = require('body-parser');
-const { request } = require('express');
-const json = require('body-parser/lib/types/json');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const axios = require('axios');
 
 //DEFINING THE EXPRESS APP
 const app = express();
@@ -24,6 +25,7 @@ const dbConfig = {
 
 const db = pgp(dbConfig);
 
+//CHECK FOR DATABASE CONNECTION
 db.connect()
   .then(obj => {
     console.log('Database connection successful'); // you can view this message in the docker compose logs
@@ -33,6 +35,26 @@ db.connect()
     console.log('ERROR:', error.message || error);
   });
 
+//TEST REGISTRATION PAGE
+app.get('/register', (req, res) => {
+  res.render('pages/register');
+});
+
+app.post('/register', async (req, res) => {
+  const email = req.body.email;
+  const username = req.body.username;
+  const country = req.body.country;
+  const hash = req.body.password; //await bcrypt.hash(req.body.password, 10);
+  console.log(req);
+  query = `INSERT INTO Users (email, username, country, password) VALUES ('${email}', '${username}', '${country}', '${hash}');`;
+
+  if(req.body.username == "") {
+    res.render('pages/register', {
+      error: true,
+      message: "Please type in a username to create an account.",
+    });
+  }
+});
 
 //SERVER LISTENING TO CLIENT REQUESTS
 app.listen(3000);
---
+++ mainProject/src/views/pages/home.ejs
@@ -0,0 +1,3 @@
+<main>
+    <h1>Home Page</h1>
+</main>
