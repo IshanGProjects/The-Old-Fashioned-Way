5a53dfa31e205f0f04bfa0d59007e3abe734ad7d
Ishan Gohil <86436938+IshanGProjects@users.noreply.github.com>
2022-11-02 21:59:43 -0600

    Adding Database Create statmenents for the Users table and the Matches Table and the Users_To_Matches

---
+++ mainProject/init_data/create.sql
@@ -0,0 +1,29 @@
+-- CREATION FOR THE Users TABLE
+DROP TABLE IF EXISTS Users CASCADE;
+CREATE TABLE Users(
+    Username VARCHAR(100) PRIMARY KEY,
+    Password VARCHAR(100) NOT NULL,
+    Email VARCHAR(100) NOT NULL,
+    Country VARCHAR(100) NOT NULL,
+    CurrencyBalance FLOAT NOT NULL,
+    TotalWins INT NOT NULL,
+    TotalLosses INT NOT NULL
+);
+
+-- CREATION FOR THE Matches TABLE
+DROP TABLE IF EXISTS Matches CASCADE;
+CREATE TABLE Matches(
+    MatchID INT PRIMARY KEY,
+    MatchCaption VARCHAR(280),
+    Victor VARCHAR(100),
+    Wager FLOAT
+);
+
+
+--RELATION TABLE:
+DROP TABLE IF EXISTS Users_To_Matches;
+CREATE TABLE Users_To_Matches(
+    Username VARCHAR(100) NOT NULL REFERENCES Users (Username),
+    MatchID INT NOT NULL REFERENCES Matches (MatchID)
+);
+
